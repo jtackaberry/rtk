jtackaberry/rtk

commit 263e15bad4bab7d9af8da5a64ea2f3738cb338e4

Fix modal release bug when touchscroll is enabled
When a modal widget (such as rtk.Popup) is set during a mousedown
handler, when touchscreen is enabled (in which the touch activate event
is mouse up) the subsequent mouseup event would cause the modal
widget(s) to immediately release.

This skips the release request if the modal widgets were set in the same
tick as the touch activate event occurred.

rtk/window.lua
@@ -1938,6 +1938,7 @@ function rtk.Window:_update()
                 end
                 -- Record current time the button was pressed
                 buttonstate.time = now
+                buttonstate.tick = rtk.tick
                 -- Also keep track of the order the buttons were pressed, so when we
                 -- generate a simulated mousedown later we can use an appropriate value
                 -- for event.button.
@@ -2046,14 +2047,34 @@ function rtk.Window:_update()
         -- last event generated above.
         if not event.handled and rtk.is_modal() and
            ((focus_changed and not self.is_focused) or event.type == rtk._touch_activate_event) then
-            for _, widget in pairs(rtk._modal) do
-                widget:_release_modal(event)
+            for _, info in pairs(rtk._modal) do
+                local widget, modaltick = table.unpack(info)
+                -- Don't ask the widget to release modal if it was set to modal in the
+                -- same tick as the most recent mousedown event (which is actually the
+                -- current event when touchscroll is disabled).  This can happen, for
+                -- example, when touchscroll is enabled and an rtk.Popup is opened in
+                -- response to mousedown.  We don't want the mouseup to immediately close
+                -- when mouseup occurs.
+                local state = rtk.mouse.state[event.button]
+                -- If state is nil, then this must have been a loss of focus by the window rather
+                -- than a mouse click.  In this case we always want to release modal.
+                local downtick = state and state.tick
+                if modaltick ~= downtick then
+                    widget:_release_modal(event)
+                end
             end
         end
         if not event.handled and rtk.focused and event.type == rtk._touch_activate_event then
             -- Unhandled click, blur focused widget.
             rtk.focused:blur(event, nil)
         end
+        if event.type == rtk.Event.MOUSEUP then
+            rtk.mouse.state[event.button] = nil
+            if event.buttons == 0 then
+                -- Clear on mouseup if no more buttons are held.  We want to do this *after*
+                -- _handle_event() above to ensure mouseup handlers can test rtk._pressed_widgets
+                -- to see if a widget receiving mouseup had previously received mousedown.
+                rtk._pressed_widgets = nil
             end
         end
         -- If the current cursor is undefined, it means no widgets requested a custom cursor,
@@ -2125,13 +2146,6 @@ function rtk.Window:_handle_window_event(event, now, suppress)
     -- log.info('handle window: %s %s', self.title, event)
     assert(event.type ~= rtk.Event.MOUSEDOWN or event.button ~= 0)
     if event.type == rtk.Event.MOUSEUP then
-        rtk.mouse.state[event.button] = nil
-        if event.buttons == 0 then
-            -- Clear on mouseup if no more buttons are held.  We want to do this *after*
-            -- _handle_event() above to ensure mouseup handlers can test rtk._pressed_widgets
-            -- to see if a widget receiving mouseup had previously received mousedown.
-            rtk._pressed_widgets = nil
-        end
         self._last_mouseup_time = event.time
         rtk._drag_candidates = nil
         if rtk.dnd.dropping then

rtk/core.lua
@@ -1135,7 +1135,7 @@ function rtk.add_modal(...)
     end
     local widgets = {...}
     for _, widget in ipairs(widgets) do
-        rtk._modal[widget.id] = widget
+        rtk._modal[widget.id] = {widget, rtk.tick}
     end
 end
 

commit 37b56887a5d2cd41121e3830ab2730982ee1dac3

Fix button state not always clearing on mouseup
Regression from 263e15b

rtk/widget.lua
@@ -2835,6 +2835,10 @@ function rtk.Widget:_deferred_mousedown(event, x, y)
     local mousedown_handled = event:get_button_state('mousedown-handled')
     if not mousedown_handled and event:is_widget_pressed(self) and not event:get_button_state(self) then
         local downevent = event:clone{type=rtk.Event.MOUSEDOWN, simulated=true, x=x or event.x, y=y or event.y}
+        -- Overwrite the tick that rtk.Window would have registered for the real mousedown
+        -- to the current tick.  Ensures any modal widgets added by the mousedown handler
+        -- we're about to invoke will not get summarily released,
+        event:set_button_state('tick', rtk.tick)
         if self:_handle_mousedown(downevent) then
             -- Ensure mouseup gets handled so the window doesn't blur us. It's intentional
             -- that we handle the original event here, because we want to prevent

rtk/window.lua
@@ -1971,6 +1971,10 @@ function rtk.Window:_update()
             log.warning('rtk: no event for mousecap=%s which indicates an internal rtk bug', gfx.mouse_cap)
         end
     end
+
+    -- XXX: from this point forward, event mustn't be meddled with.  We need to ensure
+    -- MOUSEUP events remain intact in order to properly clear button state later on.
+
     if rtk._soon_funcs then
         rtk._run_soon()
     end
@@ -1996,7 +2000,10 @@ function rtk.Window:_update()
                     -- event through the widget tree, in case a mouseup or onclick handler
                     -- will change the cursor of the widget the mouse is over.
                     calc.cursor = rtk.mouse.cursors.UNDEFINED
-                    self:_handle_window_event(self:_get_mousemove_event(true), now)
+                    -- Generate a fresh event for this so as not to mangle the existing event
+                    -- which is inspected later on.
+                    local tmp = event:clone{type=rtk.Event.MOUSEMOVE, simulated=true}
+                    self:_handle_window_event(tmp, now)
                 end
             end
             -- A no-op if the size hasn't changed.