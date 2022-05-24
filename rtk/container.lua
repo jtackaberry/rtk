jtackaberry/rtk

commit 11b28e16cc2be23a2bc4740b27e59b51c1835b5a

Fire keypress for containers with focused children
If a focused widget doesn't handle a key event then invoke the handler
on the parent container(s).

rtk/container.lua
@@ -344,6 +344,14 @@ function rtk.Container:_sync_child_refs(child, action)
     end
 end
 
+function rtk.Container:_set_focused_child(child)
+    local w = self
+    while w do
+        w._focused_child = child
+        w = w.parent
+    end
+end
+
 function rtk.Container:_validate_child(child)
     assert(rtk.isa(child, rtk.Widget), 'object being added to container is not subclassed from rtk.Widget')
 end
@@ -360,6 +368,9 @@ function rtk.Container:_reparent_child(child)
     -- changed so it is able to request a reflow from the window.
     child.window = self.window
     self:_sync_child_refs(child, 'add')
+    if rtk.focused == child then
+        self:_set_focused_child(child)
+    end
 end
 
 function rtk.Container:_unparent_child(pos)
@@ -371,8 +382,15 @@ function rtk.Container:_unparent_child(pos)
         child.parent = nil
         child.window = nil
         self:_sync_child_refs(child, 'remove')
+        if rtk.focused == child then
+            self:_set_focused_child(nil)
+        end
         return child
     end
+end
+
+function rtk.Container:focused(event)
+    return rtk.focused == self or (event and event.type == rtk.Event.KEY and rtk.focused == self._focused_child)
 end
 
 --- Adds a widget to the container.
@@ -660,7 +678,7 @@ function rtk.Container:_handle_event(clparentx, clparenty, event, clipped, liste
     -- obscured by the container.  Also if we're dragging with mouse button
     -- pressed, the container needs to have the opportunity to serve as a drop
     -- target.
-    rtk.Widget._handle_event(self, clparentx, clparenty, event, clipped, listen)
+    return rtk.Widget._handle_event(self, clparentx, clparenty, event, clipped, listen)
 end
 
 

rtk/entry.lua
@@ -852,7 +852,7 @@ function rtk.Entry:_handle_keypress(event)
 end
 
 function rtk.Entry:_get_touch_activate_delay(event)
-    if self:focused() then
+    if self:focused(event) then
         -- If focused, don't delay touch activation, otherwise it frustrates drag-selections.
         return 0
     else
@@ -862,7 +862,7 @@ end
 
 
 function rtk.Entry:_handle_dragstart(event)
-    if not self:focused() or event.button ~= rtk.mouse.BUTTON_LEFT then
+    if not self:focused(event) or event.button ~= rtk.mouse.BUTTON_LEFT then
         return
     end
     -- Superclass method disables dragging so don't call it.
@@ -922,7 +922,7 @@ function rtk.Entry:_handle_doubleclick(event)
 end
 
 
-function rtk.Entry:_rendertext(x, y)
+function rtk.Entry:_rendertext(x, y, event)
     self._font:set()
     -- Drawing text onto a fully transparent image, and then blending that image over the
     -- current drawing target looks really janky.  There is almost certainly some bug with
@@ -936,7 +936,7 @@ function rtk.Entry:_rendertext(x, y)
     }
 
     self._backingstore:pushdest()
-    if self._selstart and self:focused() then
+    if self._selstart and self:focused(event) then
         local a, b  = self:get_selection_range()
         self:setcolor(rtk.theme.entry_selection_bg)
         gfx.rect(
@@ -965,7 +965,7 @@ function rtk.Entry:_draw(offx, offy, alpha, event, clipw, cliph, cltargetx, clta
 
     rtk.Widget._draw(self, offx, offy, alpha, event, clipw, cliph, cltargetx, cltargety, parentx, parenty)
     local x, y = calc.x + offx, calc.y + offy
-    local focused = self:focused()
+    local focused = self:focused(event)
     if (y + calc.h < 0 or y > cliph or calc.ghost) and not focused then
         -- Widget would not be visible on current drawing target
         return false
@@ -989,7 +989,7 @@ function rtk.Entry:_draw(offx, offy, alpha, event, clipw, cliph, cltargetx, clta
         self:_calcview()
     end
     if self._dirty_text then
-        self:_rendertext(x, y)
+        self:_rendertext(x, y, event)
     end
 
     local amul = calc.alpha * alpha

rtk/popup.lua
@@ -181,7 +181,7 @@ function rtk.Popup:initialize(attrs, ...)
 end
 
 function rtk.Popup:_handle_event(clparentx, clparenty, event, clipped, listen)
-    rtk.Viewport._handle_event(self, clparentx, clparenty, event, clipped, listen)
+    listen = rtk.Viewport._handle_event(self, clparentx, clparenty, event, clipped, listen)
     -- Ensure we mark all activation events (MOUSEUP when touchscroll is enabled,
     -- MOUSEDOWN otherwise) within the viewport as handled to ensure we don't forfeit
     -- focus and close the popup when clicking within the viewport. Setting autofocus
@@ -189,6 +189,7 @@ function rtk.Popup:_handle_event(clparentx, clparenty, event, clipped, listen)
     if event.type == rtk._touch_activate_event and self.mouseover then
         event:set_handled(self)
     end
+    return listen
 end
 
 function rtk.Popup:_reflow(boxx, boxy, boxw, boxh, fillw, fillh, clampw, clamph, rescale, viewport, window, greedyw, greedyh)

rtk/slider.lua
@@ -778,7 +778,7 @@ function rtk.Slider:_animate_thumb_overlays(event, focused, force)
         return
     end
     if focused == nil then
-        focused = self.window.is_focused and self:focused()
+        focused = self.window.is_focused and self:focused(event)
     end
     for i = 1, #self._thumbs do
         local dst = nil

rtk/viewport.lua
@@ -242,6 +242,9 @@ function rtk.Viewport:_handle_attr(attr, value, oldval, trigger, reflow, sync)
             oldval.parent = nil
             oldval.window = nil
             self:_sync_child_refs(oldval, 'remove')
+            if rtk.focused == oldval then
+                self:_set_focused_child(nil)
+            end
         end
         if value then
             -- Similar to rtk.Container:_reparent_child()
@@ -249,6 +252,9 @@ function rtk.Viewport:_handle_attr(attr, value, oldval, trigger, reflow, sync)
             value.parent = self
             value.window = self.window
             self:_sync_child_refs(value, 'add')
+            if rtk.focused == value then
+                self:_set_focused_child(value)
+            end
         end
     elseif attr == 'bg' then
         -- If no bg is specified, use the window background.  It's not guaranteed
@@ -272,12 +278,19 @@ function rtk.Viewport:_handle_attr(attr, value, oldval, trigger, reflow, sync)
     return true
 end
 
+-- For the next three methods, we hijack rtk.Container's implementations, which don't
+-- depend on anything not already available in rtk.Viewport
 function rtk.Viewport:_sync_child_refs(child, action)
-    -- Hijack the rtk.Container implementation, which doesn't depend on anything not
-    -- already available in rtk.Viewport.
     return rtk.Container._sync_child_refs(self, child, action)
 end
 
+function rtk.Viewport:_set_focused_child(child)
+    return rtk.Container._set_focused_child(self, child)
+end
+
+function rtk.Viewport:focused(event)
+    return rtk.Container.focused(self, event)
+end
 
 function rtk.Viewport:_reflow(boxx, boxy, boxw, boxh, fillw, fillh, clampw, clamph, uiscale, viewport, window, greedyw, greedyh)
     local calc = self.calc
@@ -617,7 +630,7 @@ function rtk.Viewport:_handle_event(clparentx, clparenty, event, clipped, listen
             event:set_handled(self)
         end
     end
-    rtk.Widget._handle_event(self, clparentx, clparenty, event, clipped, listen)
+    return rtk.Widget._handle_event(self, clparentx, clparenty, event, clipped, listen)
 end
 
 function rtk.Viewport:_get_vscrollbar_client_pos()

rtk/widget.lua
@@ -1544,16 +1544,22 @@ end
 
 --- Check if the widget currently has focus.
 --
--- This simply just checks is this widget is the same as `rtk.focused`.  In
--- rtk, exactly one (or zero) widgets can grab focus.  A container that holds
--- a focused widget is not itself considered focused.
+-- This simply just checks is this widget is the same as `rtk.focused`.  In rtk, exactly
+-- one (or zero) widgets can grab focus.  A container that holds a focused widget is not
+-- itself considered focused, but depending on the type of event, it may invoke event
+-- handlers for certain events when one of its children has focus.
 --
--- A widget obtains focus when it is `autofocus` and the mouse clicks on it
--- (and the widget is using the default `onmousedown()` handler), or if
--- `focus()` is explicitly called.
+-- A widget obtains focus when it is `autofocus` and the mouse clicks on it (and the
+-- widget is using the default `onmousedown()` handler), or if `focus()` is explicitly
+-- called.
 --
+-- @tparam rtk.Event|nil event if specified, is the event which will be dispatched
+--   to event handlers if this functions returns true.  This is ignored by rtk.Widget,
+--   but subclasses can override.  For example, `rtk.Container` overrides this and
+--   considers itself focused for `rtk.Event.KEY` events when one of its children
+--   has focus.
 -- @treturn bool true if focused, false otherwise
-function rtk.Widget:focused()
+function rtk.Widget:focused(event)
     return rtk.focused == self
 end
 
@@ -1592,6 +1598,9 @@ function rtk.Widget:focus(event)
     end
     if rtk.focused == nil and self:_handle_focus(event) ~= false then
         rtk.focused = self
+        if self.parent then
+            self.parent:_set_focused_child(self)
+        end
         self:queue_draw()
         return true
     end
@@ -1614,12 +1623,15 @@ end
 -- @treturn bool true if focus was relinquished and the blur succeeded, false
 --   otherwise
 function rtk.Widget:blur(event, other)
-    if not self:focused() then
+    if not self:focused(event) then
         -- Widget wasn't focused
         return true
     end
     if self:_handle_blur(event, other) ~= false then
         rtk.focused = nil
+        if self.parent then
+            self.parent:_set_focused_child(nil)
+        end
         self:queue_draw()
         return true
     end
@@ -2483,7 +2495,7 @@ function rtk.Widget:_get_touch_activate_delay(event)
             -- for touch scrolling.
             return 0
         end
-        return (not self:focused() and event.button == rtk.mouse.BUTTON_LEFT) and
+        return (not self:focused(event) and event.button == rtk.mouse.BUTTON_LEFT) and
                self.touch_activate_delay or rtk.touch_activate_delay
     end
 end
@@ -2575,7 +2587,7 @@ function rtk.Widget:_handle_event(clparentx, clparenty, event, clipped, listen)
                 self:_handle_dragmousemove(event, dnd.arg)
             elseif self.hovering == false then
                 -- Mousemove event over a widget that's not currently marked as hovering.
-                if event.buttons == 0 or self:focused() then
+                if event.buttons == 0 or self:focused(event) then
                     -- No mouse buttons pressed or the widget currently has focus.  We set
                     -- the widget as hovering and mark the event as handled if the
                     -- onmouseenter() handler returns true, and assuming we haven't
@@ -2691,7 +2703,7 @@ function rtk.Widget:_handle_event(clparentx, clparenty, event, clipped, listen)
                         end
                     end
                 end
-                if self:focused() then
+                if self:focused(event) then
                     -- This is a refired (simulated) mousedown event and as we were
                     -- previously focused we'll hold onto the focus by marking the event
                     -- as handled.
@@ -2790,7 +2802,7 @@ function rtk.Widget:_handle_event(clparentx, clparenty, event, clipped, listen)
     -- When touchscroll is enabled, ensure we also mark the mouseup as handled if we had
     -- previously handled mousedown and were focused as a result. This prevents rtk.Window
     -- from blurring us when touchscroll is enabled.
-    if rtk.touchscroll and event.type == rtk.Event.MOUSEUP and self:focused() then
+    if rtk.touchscroll and event.type == rtk.Event.MOUSEUP and self:focused(event) then
         if event:get_button_state('mousedown-handled') == self then
             event:set_handled(self)
             self:queue_draw()
@@ -2798,7 +2810,7 @@ function rtk.Widget:_handle_event(clparentx, clparenty, event, clipped, listen)
     end
     -- Key events don't depend on where the mouse is (as above) just whether we are
     -- focused.
-    if event.type == rtk.Event.KEY and not event.handled and self:focused() then
+    if event.type == rtk.Event.KEY and not event.handled and self:focused(event) then
         if self:_handle_keypress(event) then
             event:set_handled(self)
             self:queue_draw()
@@ -3033,7 +3045,7 @@ function rtk.Widget:_handle_mousedown(event)
     if ok ~= false then
         if self.calc.autofocus then
             self:focus(event)
-            return ok or self:focused()
+            return ok or self:focused(event)
         else
             return ok or false
         end

commit 17af00bae4e9d3a9289cc563bf55f3a21bb27794

Fix container focus regression from 11b28e1

rtk/container.lua
@@ -390,7 +390,12 @@ function rtk.Container:_unparent_child(pos)
 end
 
 function rtk.Container:focused(event)
-    return rtk.focused == self or (event and event.type == rtk.Event.KEY and rtk.focused == self._focused_child)
+    return rtk.focused == self or (
+        -- If the event is a keypress and the focused widget is a child, we consider
+        -- containers focused as well.  This means keypress events not handled by
+        -- widgets will be dispatched to their parent container(s).
+        event and event.type == rtk.Event.KEY and rtk.focused and rtk.focused == self._focused_child
+    )
 end
 
 --- Adds a widget to the container.