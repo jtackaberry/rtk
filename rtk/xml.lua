40eba2bbe9628abaacb01b08f8b950ab9ca69035
Jason Tackaberry <tack@urandom.ca>
2023-09-06 14:46:51 -0400

    Update copyright statement on all files

--- rtk/xml.lua
+++ rtk/xml.lua
@@ -1,4 +1,4 @@
--- Copyright 2022 Jason Tackaberry
+-- Copyright 2022-2023 Jason Tackaberry
 --
 -- Licensed under the Apache License, Version 2.0 (the "License");
 -- you may not use this file except in compliance with the License.
